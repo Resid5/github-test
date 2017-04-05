ca8a3c7ccb17c0349d6e786b1fc42a201abdcf35
Resid5 <residrashid@o2.pl>
2017-04-05 16:14:29 +0200

    nowe gonwo

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication58
         static void Main(string[] args)
         {
             Console.WriteLine("githubtest");
-
+            Console.ReadLine();
 
         }
     }
